de6e177b1af52ec0a1b8a91320b7ea2e6f851f7f
MrShinigami13 <omol@kth.se>
2017-02-04 16:34:41 +0100

    fixat error

--- prime.c
+++ prime.c
@@ -16,26 +16,26 @@ int intcheck(double variable)
 }
 
 int is_prime(int n){
-	int prime;	//variabel för att retunera 0 eller 1 beroende om det är ett primtal eller inte
+	double prime;	//variabel för att retunera 0 eller 1 beroende om det är ett primtal eller inte
 	double n_sqrt;
 	double n_new = n;
 	n_sqrt = sqrt(n_new);
-	for (int i = 0; i <= n_sqrt; i++) // lite nya komentarer för att på en ny commimt
+	for (int i = 0; i <= n_sqrt; i++) // här görs räkngen om det är primtal
 	{
 		
-		double prime_check = n / i;
+		double prime_check = n / i;	// check för att kolla om det är ett primtal
 		prime = intcheck(prime_check);
 	}
 
 
 
-	if(prime == 1)
+	if(prime == 1)	//kollar om det är ett primtal och skrimer ut 0 eller 1 beroente på resultatet
 	{
-		return 1;
+		return 1; // ja det är primtal
 	}
 	else
 	{
-		return 0;
+		return 0;	// nej det är inte ett primtal
 	}
   
 }
