8f6697baa69f34d89418dd2a5ebd883a83015541
MrShinigami13 <omol@kth.se>
2017-02-04 19:43:47 +0100

    uppgift 2b klar?

--- print-primes.c
+++ print-primes.c
@@ -8,8 +8,46 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define COLUMNS 6
+
+
+
+double intcheck(double variable)	// för att kolla om det är jämt delbart
+{
+	int intpartofvariable = (int)variable;
+	double return_value = ((double)intpartofvariable / variable);
+	return (return_value);
+}
+
+int is_prime(double n) {
+	double prime = 1;	//variabel för att retunera 0 eller 1 beroende om det är ett primtal eller inte
+	double n_sqrt;
+	n_sqrt = sqrt(n);
+	for (double i = 2; i <= n_sqrt; i++) // här görs räkngen om det är primtal
+	{
+
+		double prime_check = (n / i);	// check för att kolla om det är ett primtal
+		prime = intcheck(prime_check);
+		if (prime == 1)
+		{
+			break;
+		}
+	}
+
+
+
+	if (prime != 1)	//kollar om det är ett primtal och skrimer ut 0 eller 1 beroente på resultatet
+	{
+		return 1; // ja det är primtal
+	}
+	else
+	{
+		return 0;	// nej det är inte ett primtal
+	}
+
+}
 int calc = 1;
 
 void print_number(int n)
@@ -40,7 +78,12 @@ void print_primes(int n){
 	for (int i = 1; i < n; i++)
 	{
 		
+		int return_value = is_prime(i);
+		if (return_value == 1)
+		{
 			print_number(i);
+		}
+			
 		
 	}
 }
